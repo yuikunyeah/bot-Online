fedbbbdda4eb3463494adeca5e58c7a79508b7b2
yuikunyeah <72680795+yuikunyeah@users.noreply.github.com>
2023-03-05 18:15:25 +0900

    Update code.js

--- code.js
+++ code.js
@@ -1,36 +1,21 @@
 clearInterval(setBot());
-var msg = document.getElementsByTagName("span");
+var get;
 console.log("開始");
-var msgL = msg.length; 
 var gm = prompt("gm");
 function setBot(){ 
-  setInterval(function(){   
-    var count = msg.length-msgL;   
-    msgL = msg.length;    
-    for(let i = 0;i<count;i = i+2){  
-      var readMsg = msg.item(i).textContent+msg.item(i+1).textContent;   
-      if(readMsg == gm+": &check;"){ 
-        talk("YMH:botは正常に稼働しています","ALL");    
-      }      
-      if(readMsg.indexOf(gm+": さよなら、") != -1){   
-        var banstr = gm.length+6;    
-        var userBn = readMsg.substring(banstr); 
-        console.log(userBn+"をban");       
-        var url = "https://zinro.net/m/player.php?kick="+g_players[userBn].id+"&name="+userBn;     
-        fetchO(url);        
-      }     
-      if(readMsg.indexOf(gm+": &Incpu;") != -1){     
-        fetchO("https://zinro.net/m/player.php?addCpu=1");  
-      }  
-    }  
+  setInterval(function(){ 
+    var data = fetchApi("https://zinro.net/m/api/?mode=message&id=119221");
+    talk(data,"ALL");
   },3000);
 }
-function 
-talk(url,forU){
+function talk(url,forU){
   fetchO("https://zinro.net/m/player.php?mode=message&to_user="+forU+"&message="+url);
 }
-function
-fetchO(url){
+function fetchO(url){
   fetch(url);
-  fetch("https://zinro.net/m/player.php")
+  fetch("https://zinro.net/m/player.php");
+}
+async function fetchApi(url){
+  get = (await fetch(url)).json();
+  return get;
 }
