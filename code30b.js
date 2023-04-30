b63a9a6c44ce594134ce1628b738491a21a584d4
yuikunyeah <72680795+yuikunyeah@users.noreply.github.com>
2023-04-30 21:41:50 +0900

    Update code30b.js

--- code30b.js
+++ code30b.js
@@ -8,21 +8,24 @@ function setBot(){
     var count = msg.length-msgL;   
     msgL = msg.length;    
     for(let i = 0;i<count;i = i+2){  
-      var readMsg = mあsg.item(i).textContent+msg.item(i+1).textContent;   
+      var readMsg = msg.item(i).textContent+msg.item(i+1).textContent;   
       if(readMsg == gm+": &check;"){ 
         talk("YMH:botは正常に稼働しています","ALL");    
-      }      
-      if(readMsg.indexOf(gm+": さよなら、") != -1){   
-        var banstr = gm.length+6;    
-        var userBn = readMsg.substring(banstr); 
-        console.log(userBn+"をban");       
-        var url = "https://zinro.net/m/player.php?kick="+g_players[userBn].id+"&name="+userBn;     
-        fetchO(url);        
+      }
+      if(readMsg.substr(0,gm.length).indexOf(gm) == -1){   
+        if(readMsg.indexOf("老害") != -1){
+         var userBn = "";
+         var subst;
+         while(subst != ":"){
+           userBn =　readMsg.substr(0,userBn.length+1);
+           subst = readMsg.substr(userBn.length,1);
+         }
+         console.log(userBn+"をban");       
+          var url = "https://zinro.net/m/player.php?kick="+g_players[userBn].id+"&name="+userBn; 
+          fetchO(url);  
+        }  
       }     
-      if(readMsg.indexOf(gm+": &Incpu;") != -1){     
-        fetchO("https://zinro.net/m/player.php?addCpu=1");  
-      }  
-    }  
+    }
   },3000);
 }
 function talk(url,forU){
